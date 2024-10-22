0fda40bc7120cd17dfb44456c56b139094cfc978
Bishkek25 <kunduz.qw8@gmail.com>
2024-10-22 07:08:50 -0700

    hw5

---
+++ dz5.py
@@ -0,0 +1,31 @@
+
+class InterestBearingItem:
+      pass
+
+class SavingAccount:
+     pass
+
+class BankAccount:
+     pass
+
+class CheckingAccount:
+     pass
+
+class Asset:
+     pass
+
+class RealEstate:
+     pass
+
+class InsurableItem:
+     pass
+
+class stock:
+    pass
+
+class Security:
+    pass
+
+class Bond:
+    pass
+
